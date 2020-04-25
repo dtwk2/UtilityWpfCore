4adaa52de5d922b8d5579b6bb42f192fe4259162
dtaylor-530 <d.taylor530@gmail.com>
2020-04-25 11:19:08 +0100

    Fixed VirtualisationHelper

--- UtilityWpf.CommonCore/Utility/VirtualisationHelper.cs
+++ UtilityWpf.CommonCore/Utility/VirtualisationHelper.cs
@@ -9,23 +9,25 @@ namespace UtilityWpf.Common.Utility
 {
     public static class VirtualisationHelper
     {
-        public static IObservable<IChangeSet<T>> CreateChangeSet<T>(IEnumerable<T> items, IObservable<IVirtualRequest> virtualRequests, int initialSize)
+        public static IObservable<IChangeSet<T>> CreateChangeSet<T>(IEnumerable<T> items, IObservable<IVirtualRequest> virtualRequests, int initialSize = 1000) where T : new()
+        {
+            return CreateChangeSet(items, virtualRequests, initialSize, () => Activator.CreateInstance<T>());
+        }
+
+        public static IObservable<IChangeSet<T>> CreateChangeSet<T>(IEnumerable<T> items, IObservable<IVirtualRequest> virtualRequests, int initialSize, Func<T> defaultItemGenerator)
         {
             return ObservableChangeSet.Create<T>(observableList =>
             {
-                return SelectReplacementItems(virtualRequests, observableList, initialSize, items)
+                return SelectReplacementItems(virtualRequests, observableList, initialSize, items, defaultItemGenerator)
                               .Subscribe(a =>
                               {
                                   if (a.i < a.count)
                                       observableList.ReplaceAt(a.i, a.item);
                               });
 
-                static IObservable<(int count, int i, T item)> SelectReplacementItems(IObservable<IVirtualRequest> virtualRequests, ISourceList<T> observableList, int initialSize, IEnumerable<T> items)
+                static IObservable<(int count, int i, T item)> SelectReplacementItems(IObservable<IVirtualRequest> virtualRequests, ISourceList<T> observableList, int initialSize, IEnumerable<T> items, Func<T> defaultItemGenerator)
                 {
-
-                    return SelectMultipleThrottledItems(virtualRequests, observableList, initialSize, IndexAndCacheItems(items))
-                          // Add default items to increase size of scrollviewer to that of InitialSize
-                          .Do(a => observableList.AddRange(Enumerable.Range(0, initialSize).Select(_ => Activator.CreateInstance<T>())))
+                    return SelectMultipleThrottledItems(virtualRequests, initialSize, observableList, IndexAndCacheItems(items), defaultItemGenerator)
                           .Switch()
                           .ObserveOnDispatcher()
                           .Distinct(a => a.i);
@@ -33,29 +35,33 @@ namespace UtilityWpf.Common.Utility
                     // items needs to be indexed and cached
                     static IEnumerable<(int, T)> IndexAndCacheItems(IEnumerable<T> items) => new Endless.CachedEnumerable<(int, T)>(items.Select((a, i) => (i, a)));
 
-                    static IObservable<IObservable<(int count, int i, T item)>> SelectMultipleThrottledItems(IObservable<IVirtualRequest> virtualRequests, ISourceList<T> observableList, int initialSize, IEnumerable<(int i, T pvm)> cached)
+                    static IObservable<IObservable<(int count, int i, T item)>> SelectMultipleThrottledItems(IObservable<IVirtualRequest> virtualRequests, int initialSize, ISourceList<T> observableList, IEnumerable<(int i, T pvm)> indexedItems, Func<T> defaultItemGenerator)
                     {
                         return virtualRequests
-                                    // Select parameters of items that are visible 
-                                    .Select(a => cached.Skip(a.StartIndex).Take(a.Size).LastOrDefault().i + a.Size)
-                                    // Every time the requestd items last index + size of requested items exceeds size of obsevablelist create new observable
+                                    // Select items which are outside visible range of quantity size
+                                    .Select(a => indexedItems.Skip(a.StartIndex).Take(a.Size).LastOrDefault().i + a.Size)
+                                    // Every time these items's last index exceeds size of obsevablelist emit new monad
                                     .Where(a => a >= observableList.Count)
+                                    // Add default items to increase size of scrollviewer in increments of InitialSize
+                                    .Do(a => observableList.AddRange(Enumerable.Range(0, initialSize).Select(_ => defaultItemGenerator.Invoke())))
                                     // Delay to limit possibility of error when scroll thumb moved to bottom of scroll-bar
                                     .SampleFirst(TimeSpan.FromSeconds(3))
-                                    .Select(sa => SelectThrottledItems(virtualRequests, cached, observableList));
+                                    .Select(sa => SelectVisibleThrottledItems(virtualRequests, indexedItems, observableList.Count));
 
 
                         // Throttle to delay display of items until scrolling has stopped.
-                        static IObservable<(int count, int i, T item)> SelectThrottledItems(IObservable<IVirtualRequest> virtualRequests, IEnumerable<(int i, T pvm)> cached, ISourceList<T> observableList)
+                        static IObservable<(int count, int i, T item)> SelectVisibleThrottledItems(IObservable<IVirtualRequest> virtualRequests, IEnumerable<(int i, T pvm)> cached, int count)
                         {
                             return virtualRequests
                                     .Throttle(TimeSpan.FromSeconds(0.5))
                                     .SelectMany(SelectItems);
 
-                            IObservable<(int count, int i, T item)> SelectItems(IVirtualRequest a)
-                            {
-                                return cached.Skip(a.StartIndex).Take(a.Size).Select(v => (observableList.Count, v.i, v.pvm)).ToObservable();
-                            }
+                            IEnumerable<(int count, int i, T item)> SelectItems(IVirtualRequest a)
+
+                                => cached
+                                            .Skip(a.StartIndex)
+                                            .Take(a.Size)
+                                            .Select(a => (count, a.i, a.pvm));
                         }
                     }
                 }
