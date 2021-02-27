5047326b2d132d6be6c0ea1992e2179a7037e4b5
dtaylor-530 <d.taylor530@gmail.com>
2021-02-27 10:23:13 +0000

    Improves Animation Demo

---
+++ UtilityWpf.Demo.Animation/FadeUserControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UtilityWpf.DemoAnimation
+{
+    /// <summary>
+    /// Interaction logic for FadeUserControl.xaml
+    /// </summary>
+    public partial class FadeUserControl : UserControl
+    {
+        public FadeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
