cd60d6cfa2d17cbb21b12f2d2b2d09ec9a3830b0
dtaylor-530 <d.taylor530@gmail.com>
2021-02-27 10:24:02 +0000

    Updates UtilityWpf.Interactive.Model namespace

--- UtilityWpf.Interactive/InteractiveCollectionGroupViewModel.cs
+++ UtilityWpf.Interactive/InteractiveCollectionGroupViewModel.cs
@@ -6,6 +6,7 @@ using System;
 using System.Linq;
 using System.Reactive.Linq;
 using UtilityInterface.Generic;
+using UtilityWpf.Interactive.Model;
 using UtilityWpf.Model;
 
 namespace UtilityWpf.Interactive
