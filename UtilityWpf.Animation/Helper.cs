6ef1a9efd06af8ec273605055e8cac7d385680ca
dtaylor-530 <d.taylor530@gmail.com>
2021-02-27 10:20:55 +0000

    Improves FadeControl

---
+++ UtilityWpf.Animation/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Windows.Media.Animation;
+
+namespace UtilityWpf.Animation
+{
+    public static class Helper
+    {
+        public static IObservable<EventArgs> SelectCompletions(this Storyboard storyboard) =>
+
+     Observable
+     .FromEventPattern<EventHandler, EventArgs>
+     (a => storyboard.Completed += a, a => storyboard.Completed -= a)
+     .Select(a => a.EventArgs);
+
+        public static ReplaySubject<T> ToReplaySubject<T>(this IObservable<T> source, int save = 1)
+        {
+            var replaySubject = new ReplaySubject<T>(save);
+            source.Subscribe(replaySubject);
+            return replaySubject;
+        }
+    }
+}
