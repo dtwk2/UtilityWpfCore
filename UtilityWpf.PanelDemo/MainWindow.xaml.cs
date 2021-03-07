c899cf999712f7c0fd2c8b920b351864cf050fbc
dtaylor-530 <d.taylor530@gmail.com>
2021-03-07 12:16:56 +0000

    Adds HexagonGrid

--- UtilityWpf.PanelDemo/MainWindow.xaml.cs
+++ UtilityWpf.PanelDemo/MainWindow.xaml.cs
@@ -44,6 +44,7 @@ namespace  UtilityWpf.PanelDemo
         {
 
         }
+
         public ObservableCollection<string> Collection { get; } = new ObservableCollection<string>(Enumerable.Range(0, 10).Select(a => a.ToString()));
         public string[] HalfCollection { get; } = Enumerable.Range(0, 4).Select(a => a.ToString()).ToArray();
 
