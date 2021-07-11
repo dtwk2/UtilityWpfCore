990e4a1b781a73f4255681cc7c3b63ce0029ea4b
dtaylor-530 <d.taylor530@gmail.com>
2021-07-11 19:48:56 +0100

    Updates

--- UtilityWpf.Animation/Control/FadeControl.cs
+++ UtilityWpf.Animation/Control/FadeControl.cs
@@ -13,13 +13,13 @@ namespace UtilityWpf.Animation
     public class FadeControl : ContentControl
     {
         bool flag = false;
-        Storyboard storyboardOld = new Storyboard(), storyboardNew = new Storyboard();
+        private readonly Storyboard storyboardOld = new Storyboard();
+        private Storyboard storyboardNew = new Storyboard();
         ReplaySubject<EventArgs> completions = new ReplaySubject<EventArgs>();
         UIElement newElement = null;
 
-        public static readonly DependencyProperty FadeCommandProperty = DependencyProperty.Register("FadeCommand", typeof(ICommand), typeof(FadeControl), new PropertyMetadata(default(ICommand)));
-
-
+        public static readonly DependencyProperty FadeCommandProperty = DependencyProperty.Register(
+            nameof(FadeCommand), typeof(ICommand), typeof(FadeControl), new PropertyMetadata(default(ICommand)));
 
         public int AnimationTime
         {
@@ -27,21 +27,11 @@ namespace UtilityWpf.Animation
             set { SetValue(AnimationTimeProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for AnimationTime.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty AnimationTimeProperty =
-            DependencyProperty.Register("AnimationTime", typeof(int), typeof(FadeControl), new PropertyMetadata(1000));
-
-
+        public static readonly DependencyProperty AnimationTimeProperty = DependencyProperty.Register("AnimationTime", typeof(int), typeof(FadeControl), new PropertyMetadata(1000));
 
         static FadeControl()
         {
-            //  DefaultStyleKeyProperty.OverrideMetadata(typeof(FadeControl), new FrameworkPropertyMetadata(typeof(FadeControl)));
-            FadeControl.ContentProperty.OverrideMetadata(typeof(FadeControl),
-      new FrameworkPropertyMetadata(null,
-          FrameworkPropertyMetadataOptions.None,
-          Changed, /* property changed callback */
-          null, /* coerce value callback */
-          true /* is animation prohibited */
+            FadeControl.ContentProperty.OverrideMetadata(typeof(FadeControl), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None, Changed, null, true
           ));
         }
 
@@ -67,7 +57,7 @@ namespace UtilityWpf.Animation
                 if (flag == true)
                 {
                     newElement.Opacity = 0;
-                    Content = newElement;
+                  
                     StartStory(newElement, storyboardNew, true, (int)(AnimationTime / 2d));
                 }
                 flag = false;
@@ -88,7 +78,7 @@ namespace UtilityWpf.Animation
 
         public void StartStories(UIElement oldElement, UIElement newElement)
         {
-            this.newElement = newElement;
+            this.newElement = newElement?? oldElement;
             if (flag)
                 return;
 
@@ -110,10 +100,10 @@ namespace UtilityWpf.Animation
 
             storyboard.Children.Clear();
             TimeSpan duration = TimeSpan.FromMilliseconds(animationTime);
-            DoubleAnimation fadeOutAnimation = new DoubleAnimation
-            { From = addRemove ? 0 : 1, To = addRemove ? 1 : 0, Duration = new Duration(duration), EasingFunction = new CubicEase { } };
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation(fromValue:addRemove ? 0 : 1, toValue : addRemove ? 1 : 0, duration : new Duration(duration))
+            { EasingFunction = new SineEase { } };
             Storyboard.SetTarget(fadeOutAnimation, element);
-            Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath("Opacity", addRemove ? 0 : 1));
+            Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath("Opacity"));
             storyboard.Children.Add(fadeOutAnimation);
 
             storyboard.Begin();
@@ -122,7 +112,7 @@ namespace UtilityWpf.Animation
 
     internal class FadeCommand : ICommand
     {
-        private FadeControl fadeControl;
+        private readonly FadeControl fadeControl;
 
         public event EventHandler CanExecuteChanged;
 
