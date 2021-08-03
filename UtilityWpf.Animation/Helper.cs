e530676b5e2bb00fb38bc4487c49672fa27f0079
Declan Taylor <declan.taylor@pcs-instruments.com>
2021-08-03 09:57:14 +0100

    Readds missing references to Helper class in Animation class.

--- UtilityWpf.Animation/Helper.cs
+++ UtilityWpf.Animation/Helper.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Windows;
 using System.Windows.Media;
