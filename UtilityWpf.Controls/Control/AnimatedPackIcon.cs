01b2060300b65bcfc007cf6907fbfcc76b4c1193
dtaylor-530 <d.taylor530@gmail.com>
2021-08-08 12:30:17 +0100

    Fixes AnimatedPackIcon and associated demo

--- UtilityWpf.Controls/Control/AnimatedPackIcon.cs
+++ UtilityWpf.Controls/Control/AnimatedPackIcon.cs
@@ -1,41 +1,64 @@
 ﻿using MaterialDesignThemes.Wpf;
 using System;
-using System.Reactive.Disposables;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace UtilityWpf.Controls
 {
-    public class AnimatedPackIcon : PackIcon
+    public class AnimatedPackIcon : ContentControl
     {
-        private PackIconKind newElement;
         bool flag = false;
-        private Storyboard storyboardOld = new();
+        private readonly Storyboard storyboardOld = new();
         private readonly Storyboard storyboardNew = new();
-        ReplaySubject<PackIconKind> completions = new();
-        private static PackIconKind kind;
+        readonly ReplaySubject<Unit> completions = new();
 
-
-        public static readonly DependencyProperty AnimationTimeProperty =
-    DependencyProperty.Register("AnimationTime", typeof(int), typeof(AnimatedPackIcon), new PropertyMetadata(3000));
+        public static readonly DependencyProperty AnimationTimeProperty = DependencyProperty.Register("AnimationTime", typeof(int), typeof(AnimatedPackIcon), new PropertyMetadata(3000));
+        public static readonly DependencyProperty KindProperty = DependencyProperty.Register("Kind", typeof(PackIconKind), typeof(AnimatedPackIcon), new PropertyMetadata(PackIconKind.None, Changed));
 
         static AnimatedPackIcon()
         {
-            //DefaultStyleKeyProperty.OverrideMetadata(typeof(AnimatedPackIcon), new FrameworkPropertyMetadata(typeof(AnimatedPackIcon)));
-            KindProperty.OverrideMetadata(typeof(AnimatedPackIcon),
-      new FrameworkPropertyMetadata(PackIconKind.None,
-          FrameworkPropertyMetadataOptions.None,
-          null, /* property changed callback */
-          Coerce, /* coerce value callback */
-          false /* is animation prohibited */
-          ));
         }
 
+        private static void Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is AnimatedPackIcon { flag: false } icon && e.NewValue is PackIconKind kind && e.OldValue is PackIconKind old)
+            {
+                icon.StartStories(old, kind);
+            }
+        }
+
+        public AnimatedPackIcon()
+        {
+            completions
+                .Subscribe(a =>
+                {
+                    if (flag == true)
+                    {
+                        //Opacity = 0;
+                        //this.SetValue(KindProperty, a);
+                        this.Content = new PackIcon
+                        {
+                            Width = 70,
+                            Height = 70,
+                            Margin = new Thickness(4),
+                            Kind = Kind,
+                        };
+                        StartStory(this.Content as UIElement, storyboardNew, true, (int)(AnimationTime / 2d));
+
+                    }
+                    flag = false;
+                });
+
+            storyboardOld
+                .SelectCompletions()
+                .Select(a => Unit.Default)
+                .Subscribe(completions.OnNext);
+
+        }
 
         public int AnimationTime
         {
@@ -43,77 +66,40 @@ namespace UtilityWpf.Controls
             set { SetValue(AnimationTimeProperty, value); }
         }
 
-
-        private static object Coerce(DependencyObject d, object baseValue)
+        public PackIconKind Kind
         {
-            kind = (PackIconKind)baseValue;
-
-            if (d is AnimatedPackIcon { flag: false } icon)
-            {
-                icon.StartStories(PackIconKind.None, kind);
-                return PackIconKind.None;
-            }
-
-
-            return kind;
+            get { return (PackIconKind)GetValue(KindProperty); }
+            set { SetValue(KindProperty, value); }
         }
-
 
         public override void OnApplyTemplate()
         {
-            storyboardOld.SelectCompletions().Select(a => newElement).Subscribe(completions.OnNext);
-            completions.Subscribe(a =>
-            {
-                if (flag == true)
-                {
-                    Opacity = 0;
-                    Kind = a;
-                    StartStory(this, storyboardNew, true, (int)(AnimationTime / 2d));
-                }
-                flag = false;
-
-            }, e => flag = false);
-
-
             base.OnApplyTemplate();
         }
-
 
 
         public void StartStories(PackIconKind oldElement, PackIconKind newElement)
         {
             if (flag)
                 return;
-
-            this.newElement = newElement;
-
             flag = true;
-            if (oldElement.Equals(default) == false)
-            {
-                Kind = oldElement;
-                StartStory(this, storyboardOld, false, (int)(AnimationTime / 2d));
-            }
+
+            if (this.Content is UIElement element)
+                StartStory(element, storyboardOld, false, (int)(AnimationTime / 2d));
             else
-            {
-                completions.OnNext(newElement);
-            }
+                completions.OnNext(Unit.Default);
         }
 
         public static void StartStory(UIElement element, Storyboard storyboard, bool addRemove = true, int animationTime = 500)
         {
-            if (element == null)
-                return;
-
             storyboard.Children.Clear();
             TimeSpan duration = TimeSpan.FromMilliseconds(animationTime);
-            DoubleAnimation fadeOutAnimation = new DoubleAnimation
+            DoubleAnimation fadeAnimation = new DoubleAnimation
             { From = addRemove ? 0 : 1, To = addRemove ? 1 : 0, Duration = new Duration(duration), EasingFunction = new CubicEase { } };
-            Storyboard.SetTarget(fadeOutAnimation, element);
-            Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath("Opacity", addRemove ? 0 : 1));
-            storyboard.Children.Add(fadeOutAnimation);
-
+            Storyboard.SetTarget(fadeAnimation, element);
+            Storyboard.SetTargetProperty(fadeAnimation, new PropertyPath(Control.OpacityProperty));
+            storyboard.Children.Add(fadeAnimation);
             storyboard.Begin();
         }
     }
-
 }
--- UtilityWpf.Demo.Animation.View/MaterialDesignUserControl.xaml.cs
+++ UtilityWpf.Demo.Animation.View/MaterialDesignUserControl.xaml.cs
@@ -2,24 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace UtilityWpf.DemoAnimation
+namespace UtilityWpf.Demo.Animation.View
 {
     /// <summary>
     /// Interaction logic for MaterialDesignUserControl.xaml
     /// </summary>
-    public partial class MaterialDesignUserControl : UserControl, INotifyPropertyChanged
+    public partial class MaterialDesignUserControl : UserControl
     {
         private readonly Dictionary<string, PackIconKind> values;
 
@@ -28,57 +23,31 @@ namespace UtilityWpf.DemoAnimation
             InitializeComponent();
 
             this.DataContext = this;
-            this.TextBox1.TextChanged += TextBox1_TextChanged;
             values = Enum.GetValues(typeof(PackIconKind)).Cast<PackIconKind>().GroupBy(a => a.ToString()).ToDictionary(a => a.Key.ToLowerInvariant(), a => a.First());
-            PackIconChange(values.First().Value);
+            MainItemsControl.ItemsSource = values.Values.Take(20);
         }
+    }
 
-        public string Text { get; set; }
-
-        public PackIconKind Value { get; set; }
-
-        public bool Value2 { get; set; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+    public class PackIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Text = TextBox1.Text;
-            PackIconKind value = default;
-            while (value == default && Text.Length > 1)
-            {
-                value = values.GetValueOrDefault(Text.ToLowerInvariant());
-                Text = Text.Remove(0, Text.Length - 1);
-            }
-
-            PackIconChange(value);
+            if (value is PackIconKind kind)
+                return new PackIcon
+                {
+                    Width = 70,
+                    Height = 70,
+                    Margin = new Thickness(4),
+                    Kind = kind
+                };
+            return DependencyProperty.UnsetValue;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            PackIconChange(((sender as Button)?.Content as PackIcon)?.Kind);
+            throw new NotImplementedException();
         }
 
-        private void PackIconChange(PackIconKind? value)
-        {
-
-            if (value.HasValue == false || Value == value)
-                return;
-            Value = value.Value;
-
-            // TransitionContent.Visibility = TransitionContent.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
-
-            TransitionBox.Content = new PackIcon
-            {
-                Width = 70,
-                Height = 70,
-                Margin = new Thickness(4),
-                Kind = Value
-            };
-
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Text"));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value2"));
-        }
+        public static PackIconConverter Instance { get; } = new PackIconConverter();
     }
 }
