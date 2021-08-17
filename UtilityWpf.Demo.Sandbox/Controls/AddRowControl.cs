0c4b7824f1cf9db6e9068956fba487aa9cb5e03a
dtaylor-530 <d.taylor530@gmail.com>
2021-08-17 21:58:30 +0100

    Reorganises UtilityWpf.Demo.Sandbox

---
+++ UtilityWpf.Demo.Sandbox/Controls/AddRowControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using UtilityWpf.Controls;
+
+namespace UtilityWpf.Demo.View
+{
+    public class AddRowControl : MasterControl
+    {
+        protected override void ExecuteAdd(object parameter)
+        {
+            ((Content as ListBox).DataContext as MainViewModel).Rows.Add(new RowViewModel());
+        }
+    }
+}
---
+++ UtilityWpf/Behavior/SelectedItemBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows;
+
+namespace UtilityWpf.Behavior
+{
+    /// <summary>
+    /// <a href="https://stackoverflow.com/questions/18456836/getting-selected-item-in-itemscontrol"></a>
+    /// </summary>
+    public static class SelectedItemBehavior
+    {
+        public static readonly DependencyProperty BindingProperty =
+           DependencyProperty.RegisterAttached("Binding", typeof(object), typeof(SelectedItemBehavior),
+              new FrameworkPropertyMetadata(new object(),
+                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                 SelectedItem_Changed));
+
+        public static object GetBinding(FrameworkElement frameworkElement)
+        {
+            return (object)frameworkElement.GetValue(BindingProperty);
+        }
+
+        public static void SetBinding(FrameworkElement frameworkElement, object value)
+        {
+            frameworkElement.SetValue(BindingProperty, value);
+        }
+
+        private static void SelectedItem_Changed(Object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ToggleButton toggleButton = (ToggleButton)sender;
+            toggleButton.Checked -= ToggleButtonOnChecked;
+            toggleButton.IsChecked = e.NewValue?.Equals(toggleButton.DataContext) ?? false;
+            toggleButton.Checked += ToggleButtonOnChecked;
+        }
+
+        private static void ToggleButtonOnChecked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggleButton = (ToggleButton)sender;
+            SetBinding(toggleButton, toggleButton.DataContext);
+        }
+    }
+
+}
