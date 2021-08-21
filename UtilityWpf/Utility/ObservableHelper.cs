7fc23fefdfe1d25f8e20441a768ac2aa614a8edd
dtaylor-530 <d.taylor530@gmail.com>
2021-08-21 15:40:49 +0100

    Removes ObservableHelper

--- UtilityWpf/Utility/ObservableHelper.cs
+++ UtilityWpf/Utility/ObservableHelper.cs
@@ -16,22 +16,22 @@ using UtilityWpf;
 namespace UtilityWpf
 {
 
-    public static class ObservableHelper
-    {
+   // public static class ObservableHelper
+    //{
 
-        [Obsolete("Moved to UtilityHelperEx")]
-        public static ReplaySubject<T> ToReplaySubject<T>(this IObservable<T> source, int save = 1)
-        {
-            var replaySubject = new ReplaySubject<T>(save);
-            source.Subscribe(replaySubject);
-            return replaySubject;
-        }
+        //[Obsolete("Moved to UtilityHelperEx")]
+        //public static ReplaySubject<T> ToReplaySubject<T>(this IObservable<T> source, int save = 1)
+        //{
+        //    var replaySubject = new ReplaySubject<T>(save);
+        //    source.Subscribe(replaySubject);
+        //    return replaySubject;
+        //}
 
-        [Obsolete("Moved to UtilityHelperEx")]
-        public static IObservable<T> SelectMany<T>(this IObservable<IEnumerable<T>> manyObservable)
-        //where TEnumerable : IEnumerable<T>
-        {
-            return manyObservable.SelectMany(a => a);
-        }
-    }
+        //[Obsolete("Moved to UtilityHelperEx")]
+        //public static IObservable<T> SelectMany<T>(this IObservable<IEnumerable<T>> manyObservable)
+        ////where TEnumerable : IEnumerable<T>
+        //{
+        //    return manyObservable.SelectMany(a => a);
+        //}
+   // }
 }
