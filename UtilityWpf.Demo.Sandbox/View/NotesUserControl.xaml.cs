abfe1ba9c83c504dcdd0da9d956ff86b1cf63be3
dtaylor-530 <d.taylor530@gmail.com>
2021-08-23 12:46:59 +0100

    Adds NotesUserControl

---
+++ UtilityWpf.Demo.Sandbox/View/NotesUserControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Dragablz;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Documents;
+
+namespace UtilityWpf.Demo.View
+{
+    /// <summary>
+    /// Interaction logic for DragUserControl.xaml
+    /// </summary>
+    public partial class NotesUserControl : UserControl
+    {
+        private object[] _order;
+
+        public NotesUserControl()
+        {
+            InitializeComponent();
+          
+        }
+    }
+}
---
+++ UtilityWpf.Demo.Sandbox/ViewModel/NotesViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors.Core;
+using Endless;
+
+namespace UtilityWpf.Demo.Sandbox.ViewModel
+{
+    public class NotesViewModel
+    {
+
+        public ObservableCollection<NoteViewModel> Collection { get; } = new ObservableCollection<NoteViewModel> {};
+
+
+        public System.Collections.IEnumerator NewItem { get => 0.Repeat().Select(a=> new NoteViewModel(a.ToString())).GetEnumerator(); }
+
+        private ICommand changeCommand;
+        public ICommand ChangeCommand => changeCommand ??= new ActionCommand(Change);
+
+        private void Change()
+        {
+        }
+    }
+
+    public class NoteViewModel : ReactiveObject
+    {
+        private string text;
+
+        public NoteViewModel(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get => text; set => this.RaiseAndSetIfChanged(ref text, value); }
+    }
+}
