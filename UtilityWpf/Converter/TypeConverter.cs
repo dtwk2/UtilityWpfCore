30c2036de633b5d21044bd4d9c13ea84d9dd2728
dtaylor-530 <d.taylor530@gmail.com>
2021-08-29 19:25:49 +0100

    Replaces EnumComboBox with behavior, EnumSelectorBehavior. Improves Enum related code

---
+++ UtilityWpf/Converter/TypeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UtilityWpf.Converter
+{
+    public class TypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            try
+            {
+
+                return value.GetType();
+            }
+            catch
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static EnumConverter Instance { get; } = new EnumConverter();
+    }
+}
