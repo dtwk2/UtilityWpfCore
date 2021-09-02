274ffa492d1fb6ecbd9d55683a58d87c58698474
dtaylor-530 <d.taylor530@gmail.com>
2021-09-02 17:27:04 +0100

    Moves Utility.FileSystem.Controls to Utility.Controls.FileSystem

---
+++ UtilityWpf.Controls.FileSystem/Commands/BrowserCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using UtilityWpf.Command;
+
+namespace UtilityWpf.Controls.FileSystem
+{
+    public class BrowserCommand : RelayCommand
+    { 
+        public BrowserCommand(Func<IObservable<string>> func) :base(GetAction(func, out var sub))
+        {
+            sub.Subscribe(a =>
+            {
+                RaiseTextChangedEvent(a);
+            });
+        }
+
+        static Action GetAction(Func<IObservable<string>> func, out ReplaySubject<string> replaySubject)
+        {
+            replaySubject = new(1);
+            ReplaySubject<string> rSubject1 = new(1);
+            rSubject1.Subscribe(replaySubject);
+            IDisposable? disposable = null;
+            return new Action(() =>
+            {
+                disposable?.Dispose();
+                disposable = func().Subscribe(a=>
+                rSubject1.OnNext(a));
+            });       
+        }
+
+        protected void RaiseTextChangedEvent(string text)
+        {
+            this.TextChanged?.Invoke(text);
+        }    
+
+        public event Action<string> TextChanged;      
+    }
+}
--- UtilityWpf.Controls.FileSystem/Commands/FileBrowserCommand.cs
+++ UtilityWpf.Controls.FileSystem/Commands/FileBrowserCommand.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Drawing.Imaging;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Text;
 using Microsoft.Win32;
 using ReactiveUI;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace UtilityWpf.Controls.FileSystem
 {
@@ -47,7 +51,7 @@ namespace UtilityWpf.Controls.FileSystem
             get => extension; set { extension = value; extensionChanges.OnNext(value); }
         }
         public bool IsMultiSelect
-{
+        {
             get => multiSelect; set { multiSelect = value; multiSelectChanges.OnNext(value); }
         }
 
@@ -55,17 +59,45 @@ namespace UtilityWpf.Controls.FileSystem
         {
             OpenFileDialog dlg = new();
             dlg.Multiselect = isMultiSelect;
-            //  dlg.DefaultExt = ".png";
-            //   dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
             if (extension != null)
                 dlg.DefaultExt = extension.StartsWith(".") ? extension : "." + extension;
             if (filter != null)
                 dlg.Filter = filter;
 
             bool? result = dlg.ShowDialog();
-         
+
             // if dialog closed dlg.FileName may become inaccessible; hence check for result equal to true
             return result == true ? (result, dlg.FileNames) : (result, null);
         }
+
+
+        public static (string filter, string extension) GetImageFilterAndExtension()
+        {
+            StringBuilder filter = new();
+            StringBuilder allfilter = new();
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            string sep = string.Empty;
+            foreach (var (filenameExtension, codecName) in GetCaption(codecs))
+            {
+                filter.Append($"{sep}{codecName} ({filenameExtension})|{filenameExtension}");
+                allfilter.Append(filenameExtension+";");
+                sep = "|";
+            }
+
+            filter.Append($"{sep}{"All Files"} ({"*.*"})|{"*.*"}");
+
+            var combined = "Image Files|" + allfilter.ToString() + "|" + filter.ToString();
+            return (combined, ".png"); // Default file extension 
+
+            static IEnumerable<(string fileNameExtension, string codecName)> GetCaption(ImageCodecInfo[] codecs)
+            {
+                return from codec in codecs
+                       let codecName = codec.CodecName[8..].Replace("Codec", "Files").Trim()
+                       select (codec.FilenameExtension, codecName);
+            }
+        }
+
+
     }
 }
