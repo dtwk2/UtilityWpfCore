58b5cb8ecf498edc42da727dbb4614d2c4d0f004
dtaylor-530 <d.taylor530@gmail.com>
2021-09-18 14:01:48 +0100

    Adds ResourceDictionariesUserControl to Dragablz demo for changing ResourceDictionaries at runtime

--- UtilityWpf.Demo.Common/ViewModel/TickViewModel.cs
+++ UtilityWpf.Demo.Common/ViewModel/TickViewModel.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Subjects;
 using System.Windows.Input;
 using ReactiveUI;
 
 namespace UtilityWpf.Demo.Common.ViewModel
 {
-    public class TickViewModel : ReactiveObject
-    {
+    public record struct TickChange(string Text, bool NewValue);
 
+    public class TickViewModel : ReactiveObject, IObservable<TickChange>
+    {
+        readonly ReplaySubject<TickChange> tickChanges = new();
         private bool isChecked;
 
         public TickViewModel(bool isChecked, string text = "Tick")
@@ -20,12 +19,19 @@ namespace UtilityWpf.Demo.Common.ViewModel
             Command = ReactiveCommand.Create(() =>
             {
                 IsChecked = !IsChecked;
+                tickChanges.OnNext(new TickChange(text, IsChecked));
+
             });
         }
 
         public bool IsChecked { get => isChecked; set => this.RaiseAndSetIfChanged(ref isChecked, value); }
         public string Text { get; }
 
-        public ICommand Command { get; } 
+        public ICommand Command { get; }
+
+        public IDisposable Subscribe(IObserver<TickChange> observer)
+        {
+            return tickChanges.Subscribe(observer);
+        }
     }
 }
---
+++ UtilityWpf.Demo.Dragablz/ResourceDictionariesUserControl.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Endless;
+using Microsoft.Xaml.Behaviors.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UtilityWpf.Demo.Common.ViewModel;
+
+namespace UtilityWpf.Demo.Dragablz
+{
+    /// <summary>
+    /// Interaction logic for TicksUserControl.xaml
+    /// </summary>
+    public partial class ResourceDictionariesUserControl : UserControl
+    {
+        public ResourceDictionariesUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+
+    public class ResourceDictionariesViewModel
+    {
+        private ICommand changeCommand;
+        private bool isReadOnly;
+        Subject<TickChange> subject = new();
+        Dictionary<ResourceDictionary, bool> dictionary = new();
+        public ResourceDictionariesViewModel()
+        {
+            subject.Subscribe(ad =>
+            {
+                var dict = dictionary.Keys.Single(a => a.Source.ToString() == ad.Text);
+                dictionary[dict] = ad.NewValue;
+                if (ad.NewValue)
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                else
+                    Application.Current.Resources.MergedDictionaries.Remove(dict);
+            });
+
+            Collection = new();
+            foreach (var dic in Application.Current.Resources.MergedDictionaries.Where(a => a.Source != null))
+            {
+                var tick = new TickViewModel(true, dic.Source.ToString());
+                dictionary[dic] = true;
+                tick.Subscribe(subject);
+                Collection.Add(tick);
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get => isReadOnly; set => isReadOnly = value;
+        }
+        public string Header { get; } = "Resource-Dictionaries ViewModel";
+
+        public ObservableCollection<TickViewModel> Collection { get; }
+
+
+        public ICommand ChangeCommand => changeCommand ??= new ActionCommand(Change);
+
+        private void Change()
+        {
+
+        }
+    }
+
+ 
+}
