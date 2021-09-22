9a5898288e4bd0d82a913cbcd678c2e25dbab166
dtaylor-530 <d.taylor530@gmail.com>
2021-09-22 22:16:40 +0100

    Removes MainWindow

--- UtilityWpf.Demo/App.xaml.cs
+++ UtilityWpf.Demo/App.xaml.cs
@@ -3,6 +3,12 @@ using Splat.Autofac;
 using System.Reflection;
 using System.Windows;
 using Utility.Common;
+using UtilityWpf.Controls.Master;
+using UtilityWpf.Demo.FileSystem;
+using UtilityWpf.Demo.Hybrid;
+using UtilityWpf.Demo.View.Animation;
+using UtilityWpf.Demo.View.Panels;
+using UtilityWpf.Demo.View;
 
 namespace UtilityWpf.DemoApp
 {
@@ -15,6 +21,20 @@ namespace UtilityWpf.DemoApp
         {         
             var builder = new ContainerBuilder();
             builder.UseAutofacDependencyResolver();
+
+            var a = typeof(BarUserControl);
+            var b = typeof(CornerPanelView);
+            var c = typeof(AdornerUserControl);
+            var d = typeof(FileBrowserView);
+            var e = typeof(MasterListUserControl);
+            var f = typeof(MeasurementsUserControl);
+            var g = typeof(UtilityWpf.Demo.Dragablz.NotesUserControl);
+
+            new Window
+            {
+                WindowState = WindowState.Maximized,
+                Content = new ViewsExDetailControl(new[] { c, a, b, d, e, f, g })
+            }.Show();
         }
     }
 }
