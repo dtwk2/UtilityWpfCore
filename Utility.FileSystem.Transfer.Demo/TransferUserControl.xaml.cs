575f3a8bd066c4d00bb7a7b10dfbb4448098ff42
Declan Taylor <declan.taylor@pcs-instruments.com>
2021-10-05 11:30:22 +0100

    Adds TransferUserControl

---
+++ Utility.FileSystem.Transfer.Demo/TransferUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Utility.FileSystem.Transfer.Demo
+{
+    /// <summary>
+    /// Interaction logic for TransferUserControl.xaml
+    /// </summary>
+    public partial class TransferUserControl : UserControl
+    {
+        public TransferUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
