e7b02070fa560b0d36ed4f29b869a30766c566fc
Declan Taylor <declan.taylor@pcs-instruments.com>
2021-12-01 17:16:06 +0000

    Brings BreadCrumbLib into solution

---
+++ BreadCrumb/BreadCrumbLib/BreadcrumbItem.cs
@@ -0,0 +1,52 @@
+﻿using BreadcrumbLib.Infrastructure;
+using SniffCore.Buttons;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BreadcrumbLib
+{
+    public class BreadcrumbItem : SelectableHeaderedItemsControl
+    {
+
+        private static RoutedUICommand invokeMenuCommand = new RoutedUICommand("Open menu", "InvokeMenuCommand", typeof(BreadcrumbItem));
+        private SplitButton button;
+
+        public static RoutedUICommand InvokeMenuCommand => invokeMenuCommand;
+
+
+        static BreadcrumbItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BreadcrumbItem), new FrameworkPropertyMetadata(typeof(BreadcrumbItem)));
+        }
+
+        public BreadcrumbItem()
+            : this(null)
+        { }
+
+        internal BreadcrumbItem(object parent) : base(parent)
+        {
+            CommandBindings.Add(new CommandBinding(InvokeMenuCommand, CommandBindingToMenuItem_Executed));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            button = this.GetTemplateChild("button") as SplitButton;
+            button.SizeChanged += Button_SizeChanged;
+            base.OnApplyTemplate();
+        }
+
+        private void Button_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Move the popup to the bottom-right corner of the BreadcrumbItem
+            button.PlacementRectangle = new Rect(e.NewSize.Width, e.NewSize.Height, 0, 0);
+        }
+
+        private void CommandBindingToMenuItem_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var breadcrumb = VisualTreHelperEx.FindVisualParent<Breadcrumb>(this);
+            breadcrumb.AddTrail(ParentItem, e.Parameter);
+        }
+    }
+}
---
+++ BreadCrumb/BreadCrumbLib/Infrastructure/SelectableHeaderedItemsControl.cs
@@ -0,0 +1,96 @@
+﻿using BreadcrumbLib.Infrastructure;
+using SniffCore.Buttons;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BreadcrumbLib
+{
+    public class SelectableHeaderedItemsControl : HeaderedItemsControl
+    {
+
+        private SplitButton button;
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(SelectableHeaderedItemsControl), new UIPropertyMetadata(false));
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(SelectableHeaderedItemsControl), new UIPropertyMetadata(false));
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(object), typeof(SelectableHeaderedItemsControl), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty ParentItemProperty =
+            DependencyProperty.Register("ParentItem", typeof(object), typeof(SelectableHeaderedItemsControl), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty IsFirstProperty = 
+            DependencyProperty.Register("IsFirst", typeof(bool), typeof(SelectableHeaderedItemsControl), new UIPropertyMetadata(false));
+
+        //internal static readonly DependencyProperty ViewProperty =
+        //    DependencyProperty.Register("View", typeof(BreadcrumbView), typeof(BreadcrumbItem), new UIPropertyMetadata(null));
+
+
+
+        static SelectableHeaderedItemsControl()
+        {
+            
+        }
+
+        public SelectableHeaderedItemsControl()
+            : this(null)
+        { }
+
+        internal SelectableHeaderedItemsControl(object parent)
+        {
+            //View = view;
+            ParentItem = parent;
+            IsFirst = ParentItem == null;
+   
+        }
+
+        #region properties
+        //internal BreadcrumbView View
+        //{
+        //    get => (BreadcrumbView)GetValue(ViewProperty);
+        //    set => SetValue(ViewProperty, value);
+        //}
+
+        public bool IsFirst
+        {
+            get => (bool)GetValue(IsFirstProperty);
+            set => SetValue(IsFirstProperty, value);
+        }
+
+        public object ParentItem
+        {
+            get => (object)GetValue(ParentItemProperty);
+            set => SetValue(ParentItemProperty, value);
+        }
+
+        public object Image
+        {
+            get => GetValue(ImageProperty);
+            set => SetValue(ImageProperty, value);
+        }
+
+        public bool IsSelected
+        {
+            get => (bool)GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+
+        public bool IsExpanded
+        {
+            get => (bool)GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
+        }
+
+        #endregion properties
+
+
+
+
+
+
+    }
+}
