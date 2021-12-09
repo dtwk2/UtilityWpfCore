643ca2de029e3b7b73ef5468474afd110ef3519f
dtaylor-530 <d.taylor530@gmail.com>
2021-12-09 22:45:17 +0000

    Improves Buttons demo.

--- UtilityWpf.Demo.Buttons/Infrastructure/ButtonsViewModel.cs
+++ UtilityWpf.Demo.Buttons/Infrastructure/ButtonsViewModel.cs
@@ -5,19 +5,30 @@ using UtilityWpf.Demo.Buttons.Infrastructure;
 
 namespace UtilityWpf.Demo.Buttons
 {
+    public class Number
+    {
+        public Number(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; }
+    }
+
     public class ButtonsViewModel
     {
         public ButtonsViewModel()
         {
             Data = new ObservableCollection<ButtonViewModel>
             {
-                new("1", new ActionCommand(()=>{ })),
-                new("2", ReactiveCommand.Create(()=>{ })),
-                new("3", ReactiveCommand.Create(()=>{ })),
+                new("1", new ActionCommand(()=>{ Numbers.Add(new Number(1)); })),
+                new("2", ReactiveCommand.Create(()=>{ Numbers.Add(new Number(2)); })),
+                new("3", ReactiveCommand.Create(()=>{ Numbers.Add(new Number(3)); })),
             };
         }
 
         public ObservableCollection<ButtonViewModel> Data { get; }
+
+        public ObservableCollection<Number> Numbers { get; } = new();
 
     }
 
