4f016ae88a3de2710e8596fd2bccd7693e448686
dtaylor-530 <d.taylor530@gmail.com>
2021-12-22 18:01:26 +0000

    Improves things

--- UtilityWpf.Controls/SliderItemsControl.cs
+++ UtilityWpf.Controls/SliderItemsControl.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 using UtilityHelper.NonGeneric;
 using UtilityWpf.Property;
 
@@ -70,16 +71,17 @@ namespace UtilityWpf.Controls
                        KeyRangeCollection.Add(xx);
                    }
                });
-           }, System.Windows.Threading.DispatcherPriority.Background, default)));
+           }, DispatcherPriority.Background, default)));
         });
 
-            this.Observable(nameof(ShowKeyValuePanel)).Subscribe(_ =>
-            {
-                this.Dispatcher.Invoke(() =>
+            this.Observable(nameof(ShowKeyValuePanel))
+                .Subscribe(b =>
                 {
-                    KeyValuePanel.Visibility = ((bool)_) ? Visibility.Visible : Visibility.Collapsed;
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        KeyValuePanel.Visibility = ((bool)b) ? Visibility.Visible : Visibility.Collapsed;
+                    });
                 });
-            });
         }
 
         public ObservableCollection<KeyRange> KeyRangeCollection { get; } = new ObservableCollection<KeyRange>();
@@ -164,22 +166,22 @@ namespace UtilityWpf.Controls
         }
 
         private Task<IEnumerable<KeyRange>> GetItems(IEnumerable data, string key, string value, string min, string max) => Task.Run(() =>
-           {
-               var type = data.First().GetType().GetProperties().ToDictionary(a => a.Name, a => a);
-               var keys = UtilityHelper.PropertyHelper.GetPropertyValues<string>(data, type[key]);
-               var values = data.Cast<object>().Select(_ =>
-               type[value].GetValue(_)).ToList().Select(_ => Convert.ToDouble(_));
-               var mins = min != null ? (type.TryGetValue(min, out PropertyInfo outmin) ?
-              data.Cast<object>().Select(_ =>
-               type[min].GetValue(_)).ToList().Select(_ => Convert.ToDouble(_)) : null) : null;
-               var maxs = max != null ? (type.TryGetValue(min, out PropertyInfo outmax)) ?
-                 data.Cast<object>().Select(_ =>
-               type[max].GetValue(_)).ToList().Select(_ => Convert.ToDouble(_)) : null : null;
+            {
+                var type = data.First().GetType().GetProperties().ToDictionary(a => a.Name, a => a);
+                var keys = UtilityHelper.PropertyHelper.GetPropertyValues<string>(data, type[key]);
+                var values = data.Cast<object>().Select(_ =>
+                type[value].GetValue(_)).ToList().Select(_ => Convert.ToDouble(_));
+                var mins = min != null ? (type.TryGetValue(min, out PropertyInfo outmin) ?
+               data.Cast<object>().Select(_ =>
+                type[min].GetValue(_)).ToList().Select(_ => Convert.ToDouble(_)) : null) : null;
+                var maxs = max != null ? (type.TryGetValue(min, out PropertyInfo outmax)) ?
+                  data.Cast<object>().Select(_ =>
+                type[max].GetValue(_)).ToList().Select(_ => Convert.ToDouble(_)) : null : null;
 
-               var xxx = Factory.Create(keys, values, mins, maxs);
+                var xxx = Factory.Create(keys, values, mins, maxs);
 
-               return xxx;
-           });
+                return xxx;
+            });
 
         private void Xx_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
@@ -189,7 +191,7 @@ namespace UtilityWpf.Controls
                 KeyValuePair = keyValuePair;
                 Dictionary = ItemsControl.ItemsSource.OfType<KeyRange>().ToDictionary(a => a.Key, a => a.Value);
                 RaiseValueChangedEvent(keyValuePair);
-            }, System.Windows.Threading.DispatcherPriority.Background);
+            }, DispatcherPriority.Background);
         }
 
         protected void RaiseValueChangedEvent(KeyValuePair<string, double> KeyValuePair)
