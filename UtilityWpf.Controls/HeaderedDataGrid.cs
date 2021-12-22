9f2a23b903c7920a2e7e243cecc4d2272f1a3b7b
dtaylor-530 <d.taylor530@gmail.com>
2021-12-22 18:00:31 +0000

    Improves HeaderedListBox and Adds HeaderedDataGridi

---
+++ UtilityWpf.Controls/HeaderedDataGrid.cs
@@ -0,0 +1,58 @@
+﻿using Evan.Wpf;
+using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using UtilityWpf.Utility;
+
+namespace UtilityWpf.Controls
+{
+    internal class HeaderedItemsControlEx : DependencyObject
+    {
+        public static readonly DependencyProperty ShowCountInHeaderProperty = DependencyHelper.Register<bool>();
+
+        public static IObservable<object> HeaderChanges(HeaderedItemsControl headeredItemsControl, IObservable<bool> showCountInHeader)
+        {
+            return showCountInHeader
+                .CombineLatest(
+                headeredItemsControl.WhenAnyValue(a => a.Header).WhereNotNull().DistinctUntilChanged(), headeredItemsControl.CountChanges())
+                .Select(a =>
+                {
+                    var header = Regex.Replace(a.Second?.ToString() ?? string.Empty, @"(([\w\d\s]*) ?(\(\d*\))?)", new MatchEvaluator(match =>
+                    {
+                        if (match.Groups[0].Value == (a.Second?.ToString() ?? string.Empty))
+                            return match.Groups[2].Value.TrimEnd();
+                        else if (match.Groups[0].Value == string.Empty)
+                            return a.First ? $" ({a.Third})" : string.Empty;
+                        return string.Empty;
+                    }));
+
+                    return header;
+                });
+        }
+    }
+
+    public class HeaderedDataGrid : HeaderedItemsControl
+    {
+        public static readonly DependencyProperty ShowCountInHeaderProperty = HeaderedItemsControlEx.ShowCountInHeaderProperty.AddOwner(typeof(HeaderedDataGrid), new PropertyMetadata(true));
+
+        static HeaderedDataGrid()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HeaderedDataGrid), new FrameworkPropertyMetadata(typeof(HeaderedDataGrid)));
+        }
+
+        public HeaderedDataGrid()
+        {
+            HeaderedItemsControlEx.HeaderChanges(this, this.WhenAnyValue(a => a.ShowCountInHeader))
+            .Subscribe(a => Header = a);
+        }
+
+        public bool ShowCountInHeader
+        {
+            get { return (bool)GetValue(ShowCountInHeaderProperty); }
+            set { SetValue(ShowCountInHeaderProperty, value); }
+        }
+    }
+}
---
+++ UtilityWpf/Utility/ItemsControlHelper.cs
@@ -0,0 +1,23 @@
+﻿using DynamicData;
+using ReactiveUI;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Controls;
+
+namespace UtilityWpf.Utility
+{
+    public static class ItemsControlHelper
+    {
+        public static IObservable<int> CountChanges(this ItemsControl headeredItemsControl)
+        {
+            return UtilityHelperEx.ObservableHelper.Pace(headeredItemsControl.WhenAnyValue(a => a.ItemsSource)
+                .WhereNotNull()
+                .Select(a => UtilityHelperEx.NonGeneric.ObservableHelper.ToObservable(a).ToObservableChangeSet()
+                .ToCollection()
+                .Select(a => a.Count)), TimeSpan.FromSeconds(0.3))
+                .DistinctUntilChanged()
+                .Switch();
+        }
+    }
+}
