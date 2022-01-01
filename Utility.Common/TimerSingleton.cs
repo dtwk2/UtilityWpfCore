4932e4bec8ea16eb581ef8b4d38ba8861e833034
dtaylor-530 <d.taylor530@gmail.com>
2022-01-01 09:59:19 +0000

    Adds TextBlockAttached for getting elapsed time for a given DateTime

--- Utility.Common/TimerSingleton.cs
+++ Utility.Common/TimerSingleton.cs
@@ -1,22 +1,33 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
 namespace UtilityWpf.Service
 {
     public class TimerSingleton
     {
-        public IObservable<DateTime> Time { get; private set; }
+        public IConnectableObservable<TimeSpan> Time { get; }
+        public IConnectableObservable<DateTime> Date { get; }
 
         private static TimerSingleton? instance;
 
         private TimerSingleton()
         {
             DateTime d = DateTime.Now;
+            var interval = Observable.Interval(TimeSpan.FromSeconds(5))
+                            .StartWith(default(long));
 
-            Time = Observable.Interval(TimeSpan.FromSeconds(5))
-                .Select(_ => d + TimeSpan.FromTicks(_))
-                .Publish();
+            Time = interval
+                      .Select(_ => DateTime.Now - d)
+                      .ObserveOn(RxApp.MainThreadScheduler)
+                      .Publish();
+
+            Date = interval
+                        .Select(_ => DateTime.Now)
+                        .ObserveOn(RxApp.MainThreadScheduler)
+                        .Publish();
         }
 
         public static TimerSingleton Instance => instance ??= new TimerSingleton();
---
+++ UtilityWpf/Attached/TextBlockAttached.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using UtilityWpf.Service;
+
+namespace UtilityWpf.Attached
+{
+    public class TextBlockAttached
+    {
+        public static readonly DependencyProperty TimedTextProperty = DependencyProperty.RegisterAttached(
+            "TimedText",
+            typeof(string),
+            typeof(TextBlockAttached),
+            new FrameworkPropertyMetadata(string.Empty, TimedTextPropertyChanged));
+
+        public static void SetTimedText(DependencyObject textBlock, string value)
+        {
+            textBlock.SetValue(TimedTextProperty, value);
+        }
+
+        public static string GetTimedText(DependencyObject textBlock)
+        {
+            return (string)textBlock.GetValue(TimedTextProperty);
+        }
+
+        private static Lazy<Chronic.Parser> parser = new(() => new());
+
+        private static void TimedTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not TextBlock textBlock)
+            {
+                return;
+            }
+
+            if (e.NewValue is DateTime dateTime)
+            {
+                Timer(dateTime, textBlock);
+            }
+            else if (e.NewValue is string str)
+            {
+                if (DateTime.TryParse(str, out var date))
+                {
+                    Timer(date, textBlock);
+                    return;
+                }
+                if (parser.Value.Parse(str).ToTime() is DateTime cDate)
+                {
+                    Timer(cDate, textBlock);
+                    return;
+                }
+            }
+            throw new Exception("sdf,,.ddsd");
+        }
+
+        private static void Timer(DateTime dateTime, TextBlock textBlock)
+        {
+            TimerSingleton.Instance.Date.Connect();
+
+            TimerSingleton.Instance.Date
+                .Subscribe(dateNow =>
+                {
+                    textBlock.Text = (int)(dateNow - dateTime).TotalSeconds + " (s)";
+                });
+        }
+    }
+}
