66e0d041198567d25f8f326cf9c791120e3b265a
dtaylor-530 <d.taylor530@gmail.com>
2022-01-14 22:58:58 +0000

    Adds Syntax highlighting controls behaviors and demo.

---
+++ UtilityWpf.Demo.Controls/SyntaxHighlightUserControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+
+namespace UtilityWpf.Demo.Controls
+{
+    /// <summary>
+    /// Interaction logic for HighlightUserControl.xaml
+    /// </summary>
+    public partial class HighlightUserControl : UserControl
+    {
+        public HighlightUserControl()
+        {
+            InitializeComponent();
+
+            this.JsonSyntaxHighlightTextBox.Json = @"{
+'title': 'Person',
+    'type': 'object',
+    'properties': {
+        'firstName': {
+            'type': 'string'
+        },
+        'lastName': {
+            'type': 'string'
+        },
+        'age': {
+            'description': 'Age in years',
+            'type': 'integer',
+            'minimum': 0
+        }
+    },
+    'required': ['firstName', 'lastName']
+}".Replace('\'', '"');
+            TextBox.Text = @"
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> ";
+            this.Languages.ItemsSource = ColorCode.Languages.All.ToArray();
+        }
+    }
+}
