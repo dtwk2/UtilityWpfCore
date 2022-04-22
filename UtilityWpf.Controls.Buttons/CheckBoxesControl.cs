23bf985c4ebc918bd09babfb06757802275e81f0
dtaylor-530 <d.taylor530@gmail.com>
2022-04-22 22:08:17 +0100

    Adds new control which is a combination of CheckBoxesControl and a filtered ListBox

--- UtilityWpf.Controls.Buttons/CheckBoxesControl.cs
+++ UtilityWpf.Controls.Buttons/CheckBoxesControl.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using UtilityWpf.Base;
 using UtilityWpf.Utility;
 
@@ -20,7 +19,19 @@ namespace UtilityWpf.Controls.Buttons
 
     public delegate void OutputChangedEventHandler(object sender, RoutedEventArgs args);
 
-    public class CheckBoxesControl : ListBox<CheckBox>
+    public interface IOutput
+    {
+        event OutputChangedEventHandler OutputChange;
+
+        object Output { get; set; }
+    }
+
+    public interface ICheckedPath
+    {
+        string IsCheckedPath { get; set; }
+    }
+
+    public class CheckBoxesControl : ListBox<CheckBox>, ICheckedPath, IOutput
     {
         public static readonly DependencyProperty IsCheckedPathProperty = DependencyProperty.Register("IsCheckedPath", typeof(string), typeof(CheckBoxesControl), new PropertyMetadata(null));
         public static readonly DependencyProperty OutputProperty = DependencyProperty.Register("Output", typeof(object), typeof(CheckBoxesControl));
@@ -29,6 +40,15 @@ namespace UtilityWpf.Controls.Buttons
         static CheckBoxesControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxesControl), new FrameworkPropertyMetadata(typeof(CheckBoxesControl)));
+        }
+
+        public CheckBoxesControl()
+        {
+            this.Loaded += CheckBoxesControl_Loaded;
+        }
+
+        private void CheckBoxesControl_Loaded(object sender, RoutedEventArgs e)
+        {
         }
 
         #region properties
@@ -55,47 +75,206 @@ namespace UtilityWpf.Controls.Buttons
 
         protected override void PrepareContainerForItemOverride(CheckBox element, object item)
         {
-            BindingFactory factory = new(item);
-            if (string.IsNullOrEmpty(IsCheckedPath) == false)
-                BindingOperations.SetBinding(element, System.Windows.Controls.Primitives.ToggleButton.IsCheckedProperty, factory.TwoWay(IsCheckedPath));
+            CheckBoxesHelper.Bind(element, item, this);
 
-            if (string.IsNullOrEmpty(SelectedValuePath) == false)
-            {
-                BindingOperations.SetBinding(element, TagProperty, factory.OneWay(SelectedValuePath));
-            }
-            else if (string.IsNullOrEmpty(DisplayMemberPath) == false)
-            {
-                BindingOperations.SetBinding(element, TagProperty, factory.OneWay(DisplayMemberPath));
-            }
-            else
-            {
-                throw new System.Exception($"Expected either {nameof(SelectedValuePath)} or {nameof(DisplayMemberPath)} not to be null.");
-            }
+            element.Checked += sdf33;
+            element.Unchecked += sdf33;
 
-            element.Checked += Button_Click;
-            element.Unchecked += Button_Click;
+            sdf33(this, null);
 
-            Button_Click(null, null);
-        }
-
-        protected virtual void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(SelectedValuePath) == false ||
-                string.IsNullOrEmpty(DisplayMemberPath) == false)
+            void sdf33(object sender, RoutedEventArgs? eventArgs)
             {
-                var items = GetItems().ToArray();
-                var output = items.Where(a => a is { Tag: { } tag }).ToDictionary(a => a.Tag, a => a.IsChecked);
-                if (output.Any())
+                var output = CheckBoxesHelper.ToDictionary(this);
+                if (output?.Any() == true)
                 {
-                    Output = output;
-                    RaiseEvent(new CheckedRoutedEventArgs(OutputChangeEvent, this, output));
+                    this.Output = output;
+                    this.RaiseEvent(new CheckedRoutedEventArgs(OutputChangeEvent, sender, output));
                 }
             }
         }
+    }
 
-        private IEnumerable<CheckBox> GetItems()
+    public class CheckBoxesFilteredControl : ListBox<CheckBox>, ICheckedPath
+    {
+        public static readonly DependencyProperty IsCheckedPathProperty = DependencyProperty.Register("IsCheckedPath", typeof(string), typeof(CheckBoxesFilteredControl), new PropertyMetadata(null));
+
+        static CheckBoxesFilteredControl()
         {
-            return this.Items.Cast<object>().Select(a => ItemContainerGenerator.ContainerFromItem(a)).Cast<CheckBox>();
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxesFilteredControl), new FrameworkPropertyMetadata(typeof(CheckBoxesFilteredControl)));
+        }
+
+        public CheckBoxesFilteredControl()
+        {
+            this.Loaded += CheckBoxesControl_Loaded;
+        }
+
+        private void CheckBoxesControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #region properties
+
+        public string IsCheckedPath
+        {
+            get => (string)GetValue(IsCheckedPathProperty);
+            set => SetValue(IsCheckedPathProperty, value);
+        }
+
+        #endregion properties
+
+        protected override void PrepareContainerForItemOverride(CheckBox element, object item)
+        {
+            CheckBoxesHelper.Bind(element, item, this);
+
+            element.Checked += OnChange;
+            element.Unchecked += OnChange;
+
+            OnChange(element, default);
+
+            void OnChange(object sender, RoutedEventArgs? _)
+            {
+                if (sender is CheckBox { IsChecked: true } checkbox)
+                    checkbox.Visibility = Visibility.Visible;
+                else if (sender is CheckBox { IsChecked: false } checkbox2)
+                    checkbox2.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+
+    public class CheckBoxesComboControl : ComboBox<CheckBox>, ICheckedPath, IOutput
+    {
+        public static readonly DependencyProperty IsCheckedPathProperty = DependencyProperty.Register("IsCheckedPath", typeof(string), typeof(CheckBoxesComboControl), new PropertyMetadata(null));
+        public static readonly DependencyProperty OutputProperty = DependencyProperty.Register("Output", typeof(object), typeof(CheckBoxesComboControl));
+        public static readonly RoutedEvent OutputChangeEvent = EventManager.RegisterRoutedEvent("OutputChange", RoutingStrategy.Bubble, typeof(OutputChangedEventHandler), typeof(CheckBoxesComboControl));
+
+        static CheckBoxesComboControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxesComboControl), new FrameworkPropertyMetadata(typeof(CheckBoxesComboControl)));
+        }
+
+        #region properties
+
+        public event OutputChangedEventHandler OutputChange
+        {
+            add => AddHandler(OutputChangeEvent, value);
+            remove => RemoveHandler(OutputChangeEvent, value);
+        }
+
+        public string IsCheckedPath
+        {
+            get => (string)GetValue(IsCheckedPathProperty);
+            set => SetValue(IsCheckedPathProperty, value);
+        }
+
+        public object Output
+        {
+            get => GetValue(OutputProperty);
+            set => SetValue(OutputProperty, value);
+        }
+
+        #endregion properties
+
+        protected override void PrepareContainerForItemOverride(CheckBox element, object item)
+        {
+            CheckBoxesHelper.Bind(element, item, this);
+
+            element.Checked += OnChange;
+            element.Unchecked += OnChange;
+            OnChange(this, null);
+        }
+
+        protected virtual void OnChange(object sender, RoutedEventArgs eventArgs)
+        {
+            var output = CheckBoxesHelper.ToDictionary(this);
+            if (output?.Any() == true)
+            {
+                this.Output = output;
+                this.RaiseEvent(new CheckedRoutedEventArgs(OutputChangeEvent, sender, output));
+            }
+        }
+    }
+
+    public class CheckBoxesFilteredComboControl : ComboBox<CheckBox>, ICheckedPath
+    {
+        public static readonly DependencyProperty IsCheckedPathProperty = DependencyProperty.Register("IsCheckedPath", typeof(string), typeof(CheckBoxesFilteredComboControl), new PropertyMetadata(null));
+
+        static CheckBoxesFilteredComboControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxesComboControl), new FrameworkPropertyMetadata(typeof(CheckBoxesComboControl)));
+        }
+
+        #region properties
+
+        public string IsCheckedPath
+        {
+            get => (string)GetValue(IsCheckedPathProperty);
+            set => SetValue(IsCheckedPathProperty, value);
+        }
+
+        #endregion properties
+
+        protected override void PrepareContainerForItemOverride(CheckBox element, object item)
+        {
+            CheckBoxesHelper.Bind(element, item, this);
+
+            element.Checked += OnChange;
+            element.Unchecked += OnChange;
+            OnChange(this, null);
+        }
+
+        private void OnChange(object sender, RoutedEventArgs? _)
+        {
+            if (sender is CheckBox { IsChecked: true } checkbox)
+                checkbox.Visibility = Visibility.Visible;
+            else if (sender is CheckBox { IsChecked: false } checkbox2)
+                checkbox2.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    public static class CheckBoxesHelper
+    {
+        public static void Bind(FrameworkElement element, object item, object sender)
+        {
+            if (sender is not ICheckedPath checkedPath ||
+                sender is not System.Windows.Controls.Primitives.Selector selector)
+            {
+                throw new System.Exception("sdf4 fdgdgp;p;p");
+            }
+
+            BindingFactory factory = new(item);
+            if (string.IsNullOrEmpty(checkedPath.IsCheckedPath) == false)
+                element.SetBinding(System.Windows.Controls.Primitives.ToggleButton.IsCheckedProperty, factory.TwoWay(checkedPath.IsCheckedPath));
+
+            if (string.IsNullOrEmpty(selector.SelectedValuePath) == false)
+            {
+                element.SetBinding(FrameworkElement.TagProperty, factory.OneWay(selector.SelectedValuePath));
+            }
+            else if (string.IsNullOrEmpty(selector.DisplayMemberPath) == false)
+            {
+                element.SetBinding(FrameworkElement.TagProperty, factory.OneWay(selector.DisplayMemberPath));
+            }
+            else
+            {
+                throw new System.Exception($"Expected either {nameof(System.Windows.Controls.Primitives.Selector.SelectedValuePath)} or " +
+                    $"{nameof(System.Windows.Controls.Primitives.Selector.DisplayMemberPath)} " +
+                    $"not to be null.");
+            }
+        }
+
+        public static Dictionary<object, bool?>? ToDictionary(object sender)
+        {
+            if (sender is not System.Windows.Controls.Primitives.Selector selector)
+            {
+                throw new System.Exception("sdf4 fdgdgp;p;p");
+            }
+
+            //if (string.IsNullOrEmpty(selector.SelectedValuePath) == false ||
+            //    string.IsNullOrEmpty(selector.DisplayMemberPath) == false)
+            //{
+            var items = selector.ItemsOfType<CheckBox>().ToArray();
+            var output = items.Where(a => a is { Tag: { } tag }).ToDictionary(a => a.Tag, a => a.IsChecked);
+            return output;
+            //}
         }
     }
 
